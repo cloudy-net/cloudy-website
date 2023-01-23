b3c3eee11cda7365b2683bde9e994c0edab44e80
bassieee <102551170+bassieee@users.noreply.github.com>
2023-01-23 20:21:49 +0100

    Page, text, menu updates

---
+++ cloudy-website/Controllers/PageController.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using cloudy_website.Models;
+
+namespace cloudy_website.Controllers;
+
+public class AboutController : Controller
+{
+    private readonly ILogger<AboutController> _logger;
+
+    public AboutController(ILogger<AboutController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Careers()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
