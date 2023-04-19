a1f4eba457fd1461ff764cd9d6e4737931222c35
bassieee <102551170+bassieee@users.noreply.github.com>
2023-04-19 07:58:57 +0200

    Cycler animation

--- cloudy-website/wwwroot/js/site.js
+++ cloudy-website/wwwroot/js/site.js
@@ -5,7 +5,7 @@ const hamburger = document.getElementById("hamburger");
 const closeMobileMenu = document.getElementById("closeMobileMenu");
 const mobileMenuContainer = document.getElementsByClassName("mobileMenuContainer");
 const mobileMenu = document.getElementById("mobileMenu");
-const philosophyHomePage = document.getElementsByClassName("whyCloudyDiv");
+const cycler = document.getElementById("cycler");
 
 
 // Toggle mobile nav
@@ -25,21 +25,49 @@ closeMobileMenu.addEventListener("click", () => {
 });
 
 
-// Scroll animation
+// Cycler animation
 
-function appear() {
-    for (let i = 0; i < philosophyHomePage.length; i++) {
-        var elDistanceToTop = window.pageYOffset + philosophyHomePage[i].getBoundingClientRect().top
-        if (elDistanceToTop < (window.scrollY + window.innerHeight)) {
-            philosophyHomePage[i].style.opacity = "1";
-        } else {
-            philosophyHomePage[i].style.opacity = "0";
-        };
-    };
-};
+document.addEventListener('DOMContentLoaded', function (event) {
+    // array with texts to type in typewriter
+    var dataText = ["back offices", "CRUD apps", "back offices"];
 
-document.addEventListener("scroll", () => {
-    appear();
+    // type one text in the typewriter
+    // keeps calling itself until the text is finished
+    function typeWriter(text, i, fnCallback) {
+        // chekc if text isn't finished yet
+        if (i < (text.length)) {
+            // add next character to h1
+            cycler.innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+            // wait for a while and call this function again for next character
+            setTimeout(function () {
+                typeWriter(text, i + 1, fnCallback)
+            }, 100);
+        }
+        // text finished, call callback if there is a callback function
+        else if (typeof fnCallback == 'function') {
+            // call callback after timeout
+            setTimeout(fnCallback, 700);
+        }
+    }
+    // start a typewriter animation for a text in the dataText array
+    function StartTextAnimation(i) {
+        if (typeof dataText[i] == 'undefined') {
+            setTimeout(function () {
+                StartTextAnimation(0);
+            }, 20000);
+        }
+        // check if dataText[i] exists
+        if (i < dataText[i].length) {
+            // text exists! start typewriter animation
+            typeWriter(dataText[i], 0, function () {
+                // after callback (and whole text has been animated), start next text
+                StartTextAnimation(i + 1);
+            });
+        }
+    }
+    // start the text animation
+    StartTextAnimation(0);
 });
 
 
